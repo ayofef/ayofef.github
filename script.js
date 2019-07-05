952388915209c500a882e9aa58e357e915d2765f
ayofef <46852842+ayofef@users.noreply.github.com>
2019-07-05 12:06:49 +0100

    master

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 
-var toggleButtonOff = document.querySelector('#togg-off');
-var toggleButtonOn = document.querySelector('#togg');
-var cvpage = document.querySelector('body');
+var menuButton = document.querySelector("#burger");
 
-toggleButtonOff.addEventListener("click", function(){
- cvpage.style.filter=("invert(100%)");
- cvpage.style.transition=("filter 0.5s");
- toggleButtonOff.style.display=("none");
- toggleButtonOn.style.display=("block");
-});
 
-toggleButtonOn.addEventListener("click", function(){
-  cvpage.style.filter=("none");
-  cvpage.style.transition=("filter 0.5s");
-  toggleButtonOff.style.display=("block");
-  toggleButtonOn.style.display=("none");
+var menu = document.querySelector("nav ul");
+
+
+menuButton.addEventListener("click", function(){
+
+menu.classList.toggle("open");
 });
