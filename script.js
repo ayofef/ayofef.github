cecbb26ed47017c7295e2f630d0517c9c8e71449
ayofef <46852842+ayofef@users.noreply.github.com>
2019-07-20 20:14:53 +0100

    latest

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 
 var menuButton = document.querySelector("#burger");
 
+var menuCloseButton = document.querySelector("#burger-close");
+
 
 var menu = document.querySelector("nav ul");
 
 
 menuButton.addEventListener("click", function(){
-
 menu.classList.toggle("open");
+
+if (menuButton.innerHTML === "☰") {
+    menuButton.innerHTML = "🗙";
+  } else {
+    menuButton.innerHTML = "☰";
+  }
 });
+
