7becd022018045e8e2d4ba405c093d47335aa5b9
ayofef <46852842+ayofef@users.noreply.github.com>
2019-07-26 16:12:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ var menu = document.querySelector("nav ul");
 menuButton.addEventListener("click", function(){
 menu.classList.toggle("open");
 
-if (menuButton.innerHTML === "&times;") {
+if (menuButton.innerHTML === "&#10006;") {
     menuButton.innerHTML = "☰";
   } else {
-    menuButton.innerHTML = "&times;";
+    menuButton.innerHTML = "&#10006;";
   }
 });
 
